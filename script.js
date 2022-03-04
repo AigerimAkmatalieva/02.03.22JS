31de4e5cb06ed163766826a5b32148ac07036724
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-03-04 15:00:27 +0600

    added index.html

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-document.querySelectorAll('.accordion').forEach(accordion => {
-  accordion.querySelector('accordion__header').addEventListener('click', function() {
-    accordion.classList.toggle('accordion--open');
-  })
-})
+const example1Span = document.querySelector('#example-1 span');
+example1Span.textContent = localStorage.getItem('example-1-number') ?? 0;
+document.querySelector('#example-1 button').addEventListener('click', function() {
+	example1Span.textContent++;
+	localStorage.setItem('example-1-number', example1Span.textContent)
+});
+
+const example2Span = document.querySelector('#example-2 span');
+example2Span.textContent = localStorage.getItem('example-2-number') ?? 10;
+document.querySelector('#example-2 button').addEventListener('click', function() {
+	example2Span.textContent--;
+	localStorage.setItem('example-2-number', example1Span.textContent)
+});
+
+const darkTheme = document.querySelector('input[type=checkbox]');
+darkTheme.checked = localStorage.getItem('dark-theme');
+document.body.classList.toggle('dark-theme', darkTheme.checked);
+darkTheme.addEventListener('change', function() {
+	localStorage.setItem('dark-theme', +darkTheme.checked);
+	document.body.classList.toggle('dark-theme');
+});
+
+
+
+
